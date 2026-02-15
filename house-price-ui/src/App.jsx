14ff604e1b67807c3b8393d9ff65ecd7a5de0b42
vhendoncenita18 <vhendoncenita18@gmail.com>
2026-02-15 10:08:01 +0800

    added an login page

--- house-price-ui/src/App.jsx
+++ house-price-ui/src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'    
 import reactLogo from './assets/react.svg'            
 import viteLogo from '/vite.svg'
-import './App.css'
+import Login from './pages/Login'
 
 function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-red-500">
-        Tailwind is Working 🚀
-      </h1>
-    </div>
-  )
+  return <Login />
 }
 
 export default App
