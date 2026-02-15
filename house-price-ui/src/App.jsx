81dc98595c9053477a2b69da63cbdadd8f58eb14
vhendoncenita18 <vhendoncenita18@gmail.com>
2026-02-15 09:53:40 +0800

    install react, node, tailwind

---
+++ house-price-ui/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'    
+import reactLogo from './assets/react.svg'            
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
