67db2fd069a988492a4e1e2578e8d1e5273d3d7e
vhendoncenita18 <vhendoncenita18@gmail.com>
2026-02-15 21:58:48 +0800

    added an login card and image

--- house-price-ui/src/components/NavBar.jsx
+++ house-price-ui/src/components/NavBar.jsx
@@ -1,30 +1,33 @@
 import { Link } from 'react-router-dom';
+import SlideUp from './SlideUp';
 
 export default function NavBar() {
     return (
-        <nav className="bg-gray-100 fixed shadow-md z-50 top-0 w-full h-16 flex items-center justify-between px-16">
+       
+            <nav className="bg-gray-100 fixed shadow-md z-50 top-0 left-0 w-full h-16 flex items-center justify-between px-16">
+            
+                <div className="text-2xl font-bold text-gray-800">
+                    House
+                </div>
+
+                <ul className="flex space-x-9 text-gray-600"> 
+                    <li><a href="#" className="hover:text-blue-600 transition">Home</a></li>
+                    <li><a href="#" className="hover:text-blue-600 transition">About</a></li>
+                    <li><a href="#" className="hover:text-blue-600 transition">Contact</a></li>
+                </ul>
+
+                <button className="md:hidden">
+                    ☰
+                </button>
+
+                <div>
+                    <Link to="/login">
+                        <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+                            Login
+                        </button>
+                    </Link>
+                </div>
+            </nav>
         
-            <div className="text-2xl font-bold text-gray-800">
-                House
-            </div>
-
-            <ul className="flex space-x-9 text-gray-600"> 
-                <li><a href="#" className="hover:text-blue-600 transition">Home</a></li>
-                <li><a href="#" className="hover:text-blue-600 transition">About</a></li>
-                <li><a href="#" className="hover:text-blue-600 transition">Contact</a></li>
-            </ul>
-
-            <button className="md:hidden">
-                ☰
-            </button>
-
-            <div>
-                <Link to="/login">
-                    <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
-                        Login
-                    </button>
-                </Link>
-            </div>
-        </nav>
     );
 }
--- house-price-ui/src/pages/LoginPage.jsx
+++ house-price-ui/src/pages/LoginPage.jsx
@@ -1,11 +1,82 @@
 import NavBar from "../components/NavBar";
+import LoginImage from '../assets/login-bg.jpg';
+import SlideUp from "../components/SlideUp";
 
 export default function LoginPage() {
     return (
-        <section className="bg-gray-200 min-h-screen flex items-center">
-            <div className="max-w-7xl mx-auto px-6">
+
+        <section className="relative bg-gray-200 min-h-screen flex items-center overflow-hidden">
+    
+            <img src={LoginImage} alt="Login Background" 
+            className="absolute inset-0 w-full h-full object-cover "
+            />
+            <div className="absolute inset-0 bg-black/50"></div>
+
+            <div className="relative z-10 max-w-7xl mx-auto px-6">
                 <div className="grid md:grid-cols-2 gap-10 items-center">
+                    < SlideUp delay={100} duration={700} distance={24} className="transform">
+
+                        <div className="text-white">
+                            <h1 className="text-5xl font-bold leading-tight">
+                                Welcome Back!
+                            </h1>
+                            <p className="mt-4 text-lg text-gray-300">
+                                Predict house prices using AI powered machine learning.
+                                Fast. Accurate. Smart.
+                            </p>
+                        </div>
+                    </SlideUp>
                     
+                    <SlideUp delay={200} duration={700} distance={24} className="transform">
+                        <div className="bg-gray-300 w-full p-6 rounded-2xl shadow-2xl max-w-md mx-auto mr-1">
+                            <h2 className="text-2xl text-center text-gray-800 mb-6 font-bold">
+                                Login to your account
+                            </h2>
+
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                className="w-full border rounded-lg px-3 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            />
+
+                            <input 
+                                type="password"
+                                placeholder="Password"
+                                className="w-full border rounded-lg px-3 py-2 mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                             />
+
+                            {/*Need alisdan og button or a*/}
+                             <div className="text-right text-sm text-blue-600 mb-4 cursor-pointer">
+                                Forgot password?
+                            </div>
+                            
+                            <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
+                                Login
+                            </button>
+
+                            <div className="flex items-center my-6">
+                                <div className="flex-1 h-px bg-white"></div>
+                                <span className="px-3 text-gray-700 text-sm">OR</span>
+                                <div className="flex-1 h-px bg-white"></div>
+                            </div>
+                            
+                            <button className="w-full border py-2 rounded-lg mb-3 hover:bg-gray-50 transition bg-white ">
+                                Continue with Google
+                            </button>
+
+                            <button className="w-full border py-2 rounded-lg hover:bg-gray-50 transition bg-white">
+                                Continue with Facebook
+                            </button>
+
+                            <p className="text-center text-sm mt-6">
+                        Don’t have an account?
+                        <span className="text-blue-600 cursor-pointer ml-1">
+                        Sign up
+                        </span>
+                    </p>
+
+                        </div>
+                    </SlideUp>
                 </div>
             </div>
         </section>
